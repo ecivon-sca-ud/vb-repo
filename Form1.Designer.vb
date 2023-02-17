e1aaa92289a6d120a87845b53cfd060d75a91db0
ecivon-sca-ud <121928257+ecivon-sca-ud@users.noreply.github.com>
2023-02-17 20:54:40 +0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,317 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class StringTings
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtInitial = New System.Windows.Forms.TextBox()
+        Me.txtProper = New System.Windows.Forms.TextBox()
+        Me.txtUpper = New System.Windows.Forms.TextBox()
+        Me.txtLower = New System.Windows.Forms.TextBox()
+        Me.txtRight = New System.Windows.Forms.TextBox()
+        Me.txtMid = New System.Windows.Forms.TextBox()
+        Me.txtLeft = New System.Windows.Forms.TextBox()
+        Me.txtLength = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.CmdShow = New System.Windows.Forms.Button()
+        Me.txtStart = New System.Windows.Forms.TextBox()
+        Me.txtEnd = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(-2, 38)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Initial String:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(-2, 461)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(120, 24)
+        Me.Label8.TabIndex = 3
+        Me.Label8.Text = "Proper Case:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(-2, 401)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(115, 24)
+        Me.Label7.TabIndex = 4
+        Me.Label7.Text = "Upper Case:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(-2, 339)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(115, 24)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Lower Case:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(-2, 272)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(58, 24)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Right:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(-2, 147)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 24)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Left:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(-2, 92)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 24)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Length:"
+        '
+        'txtInitial
+        '
+        Me.txtInitial.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtInitial.Location = New System.Drawing.Point(249, 38)
+        Me.txtInitial.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.txtInitial.Name = "txtInitial"
+        Me.txtInitial.Size = New System.Drawing.Size(247, 26)
+        Me.txtInitial.TabIndex = 1
+        '
+        'txtProper
+        '
+        Me.txtProper.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtProper.Location = New System.Drawing.Point(249, 461)
+        Me.txtProper.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.txtProper.Name = "txtProper"
+        Me.txtProper.Size = New System.Drawing.Size(247, 26)
+        Me.txtProper.TabIndex = 8
+        '
+        'txtUpper
+        '
+        Me.txtUpper.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUpper.Location = New System.Drawing.Point(252, 401)
+        Me.txtUpper.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.txtUpper.Name = "txtUpper"
+        Me.txtUpper.Size = New System.Drawing.Size(244, 26)
+        Me.txtUpper.TabIndex = 7
+        '
+        'txtLower
+        '
+        Me.txtLower.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLower.Location = New System.Drawing.Point(249, 339)
+        Me.txtLower.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.txtLower.Name = "txtLower"
+        Me.txtLower.Size = New System.Drawing.Size(247, 26)
+        Me.txtLower.TabIndex = 6
+        '
+        'txtRight
+        '
+        Me.txtRight.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtRight.Location = New System.Drawing.Point(249, 272)
+        Me.txtRight.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.txtRight.Name = "txtRight"
+        Me.txtRight.Size = New System.Drawing.Size(247, 26)
+        Me.txtRight.TabIndex = 5
+        '
+        'txtMid
+        '
+        Me.txtMid.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtMid.Location = New System.Drawing.Point(249, 213)
+        Me.txtMid.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.txtMid.Name = "txtMid"
+        Me.txtMid.Size = New System.Drawing.Size(247, 26)
+        Me.txtMid.TabIndex = 4
+        '
+        'txtLeft
+        '
+        Me.txtLeft.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLeft.Location = New System.Drawing.Point(249, 154)
+        Me.txtLeft.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.txtLeft.Name = "txtLeft"
+        Me.txtLeft.Size = New System.Drawing.Size(102, 26)
+        Me.txtLeft.TabIndex = 3
+        '
+        'txtLength
+        '
+        Me.txtLength.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtLength.Location = New System.Drawing.Point(249, 98)
+        Me.txtLength.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.txtLength.Name = "txtLength"
+        Me.txtLength.Size = New System.Drawing.Size(116, 26)
+        Me.txtLength.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(-2, 208)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(46, 24)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Mid:"
+        '
+        'CmdShow
+        '
+        Me.CmdShow.BackColor = System.Drawing.Color.Red
+        Me.CmdShow.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CmdShow.Location = New System.Drawing.Point(249, 508)
+        Me.CmdShow.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.CmdShow.Name = "CmdShow"
+        Me.CmdShow.Size = New System.Drawing.Size(102, 68)
+        Me.CmdShow.TabIndex = 9
+        Me.CmdShow.Text = "&Show"
+        Me.CmdShow.UseVisualStyleBackColor = False
+        '
+        'txtStart
+        '
+        Me.txtStart.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtStart.Location = New System.Drawing.Point(513, 100)
+        Me.txtStart.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.txtStart.Name = "txtStart"
+        Me.txtStart.Size = New System.Drawing.Size(109, 26)
+        Me.txtStart.TabIndex = 10
+        '
+        'txtEnd
+        '
+        Me.txtEnd.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtEnd.Location = New System.Drawing.Point(513, 154)
+        Me.txtEnd.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.txtEnd.Name = "txtEnd"
+        Me.txtEnd.Size = New System.Drawing.Size(109, 26)
+        Me.txtEnd.TabIndex = 11
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(423, 78)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(51, 48)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Start:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(423, 156)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(50, 24)
+        Me.Label10.TabIndex = 13
+        Me.Label10.Text = "End:"
+        '
+        'StringTings
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(974, 589)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.txtEnd)
+        Me.Controls.Add(Me.txtStart)
+        Me.Controls.Add(Me.CmdShow)
+        Me.Controls.Add(Me.txtLength)
+        Me.Controls.Add(Me.txtLeft)
+        Me.Controls.Add(Me.txtMid)
+        Me.Controls.Add(Me.txtRight)
+        Me.Controls.Add(Me.txtLower)
+        Me.Controls.Add(Me.txtUpper)
+        Me.Controls.Add(Me.txtProper)
+        Me.Controls.Add(Me.txtInitial)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+        Me.Name = "StringTings"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtInitial As TextBox
+    Friend WithEvents txtProper As TextBox
+    Friend WithEvents txtUpper As TextBox
+    Friend WithEvents txtLower As TextBox
+    Friend WithEvents txtRight As TextBox
+    Friend WithEvents txtMid As TextBox
+    Friend WithEvents txtLeft As TextBox
+    Friend WithEvents txtLength As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents CmdShow As Button
+    Friend WithEvents txtStart As TextBox
+    Friend WithEvents txtEnd As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+End Class
